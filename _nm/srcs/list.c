02bfb3ea855dd5c969d16cc767cef687442f1fa1
jessyefuster <jfuster@student.42.fr>
2018-03-12 22:48:15 +0100

    Gestion des symboles dont le n_sect est à 0

--- _nm/srcs/list.c
+++ _nm/srcs/list.c
@@ -6,7 +6,7 @@
 /*   By: jessye <jessye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/16 16:05:21 by jfuster           #+#    #+#             */
-/*   Updated: 2018/03/07 00:34:45 by jessye           ###   ########.fr       */
+/*   Updated: 2018/03/12 22:46:27 by jessye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,9 @@ t_symbols	*new_node(t_filetype_t file_type, void *symbol, char *string_table)
 	new->name = "bad string index";
 	if (F_IS_32(file_type))
 	{
-		if (valid_addr(string_table + S_32(((struct nlist *)symbol)->n_un.n_strx, file_type)))
+		if (S_32(((struct nlist *)symbol)->n_un.n_strx, file_type) == 0)
+			new->name = "";
+		else if (valid_addr(string_table + S_32(((struct nlist *)symbol)->n_un.n_strx, file_type)))
 			new->name = string_table + S_32(((struct nlist *)symbol)->n_un.n_strx, file_type);
 		new->value = S_32(((struct nlist *)symbol)->n_value, file_type);
 		new->type = ((struct nlist *)symbol)->n_type;
@@ -68,7 +70,9 @@ t_symbols	*new_node(t_filetype_t file_type, void *symbol, char *string_table)
 	}
 	else
 	{
-		if (valid_addr(string_table + S_32(((struct nlist_64 *)symbol)->n_un.n_strx, file_type)))
+		if (S_32(((struct nlist_64 *)symbol)->n_un.n_strx, file_type) == 0)
+			new->name = "";
+		else if (valid_addr(string_table + S_32(((struct nlist_64 *)symbol)->n_un.n_strx, file_type)))
 			new->name = string_table + S_32(((struct nlist_64 *)symbol)->n_un.n_strx, file_type);
 		new->value = S_64(((struct nlist_64 *)symbol)->n_value, file_type);
 		new->type = ((struct nlist_64 *)symbol)->n_type;
