8caa7cc3550bdd1840b97155cb72ca9280dfd007
Jessye FUSTER <jfuster@e1r4p9.42.fr>
2018-12-30 15:09:29 +0100

    nm - mise a jour du tri

--- srcs/nm/srcs/list.c
+++ srcs/nm/srcs/list.c
@@ -6,7 +6,7 @@
 /*   By: jfuster <jfuster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/16 16:05:21 by jfuster           #+#    #+#             */
-/*   Updated: 2018/12/19 16:25:00 by jfuster          ###   ########.fr       */
+/*   Updated: 2018/12/30 15:00:39 by jfuster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,26 +61,65 @@ t_symbols		*new_node(t_file *file, void *s, char *st)
 **	note : this function handles endianess
 */
 
+// enum e_status	store_symbol(t_file *file, t_symbols **symbols, void *symbol,
+// 			char *string_table)
+// {
+// 	t_symbols		*new;
+// 	t_symbols		*ptr;
+
+// 	if ((new = new_node(file, symbol, string_table)) == NULL)
+// 		return (program_error("Malloc error", __FILE__, __LINE__));
+// 	ptr = (*symbols);
+// 	if ((*symbols) == NULL || (ft_strcmp((*symbols)->name, new->name) >= 0))
+// 	{
+// 		new->next = (*symbols);
+// 		(*symbols) = new;
+// 	}
+// 	else
+// 	{
+// 		while (ptr->next != NULL && (ft_strcmp(ptr->next->name, new->name) < 0))
+// 			ptr = ptr->next;
+// 		new->next = ptr->next;
+// 		ptr->next = new;
+// 	}
+// 	return (S_SUCCESS);
+// }
+
+// while (ptr->next != NULL && (comp < 0 || (comp == 0 && (new->value > ptr->next->value))))
+
+// ft_utob
+// displai ft_error fstat handle main nm symbol types
+
 enum e_status	store_symbol(t_file *file, t_symbols **symbols, void *symbol,
 			char *string_table)
 {
 	t_symbols		*new;
 	t_symbols		*ptr;
+	int				comp;
 
 	if ((new = new_node(file, symbol, string_table)) == NULL)
 		return (program_error("Malloc error", __FILE__, __LINE__));
 	ptr = (*symbols);
-	if ((*symbols) == NULL || (ft_strcmp((*symbols)->name, new->name) >= 0))
+	// printf("new node name: %s\n", new->name);
+	if ((*symbols) != NULL)
+		comp = ft_strcmp(new->name, (*symbols)->name);
+	if ((*symbols) == NULL || (comp < 0 || (comp == 0 && new->value < (*symbols)->value)))
 	{
+		// printf("  insert before '%s'\n\n", ((*symbols) == NULL ? NULL : (*symbols)->name));
 		new->next = (*symbols);
 		(*symbols) = new;
 	}
 	else
 	{
-		while (ptr->next != NULL && (ft_strcmp(ptr->next->name, new->name) < 0))
+		// while (ptr->next != NULL && (comp < 0 || (comp == 0 && (new->value > ptr->next->value))))
+		// if (ptr->next != NULL)
+		// 	comp = ft_strcmp(new->name, ptr->next->name);
+		while (ptr->next != NULL && (ft_strcmp(new->name, ptr->next->name) > 0 || (ft_strcmp(new->name, ptr->next->name) == 0 && new->value > ptr->next->value)))
 			ptr = ptr->next;
+		// printf("  insert after '%s'\n\n", ptr->name);
 		new->next = ptr->next;
 		ptr->next = new;
 	}
 	return (S_SUCCESS);
 }
+
